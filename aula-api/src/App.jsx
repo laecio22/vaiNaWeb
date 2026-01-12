3ce8d69ed9aa3f2128ab50f6bd3d904e79f489f5
laecio22 <laecon22@hotmail.com>
2026-01-12 14:45:18 -0300

    feat:inicio  implementação efeito colateral

--- aula-api/src/App.jsx
+++ aula-api/src/App.jsx
@@ -1,4 +1,12 @@
+import { useState, useEffect } from "react";
+import  axios  from 'axios'
+
 const App = () => {
+  const [produtos, setProdutos] =  useState([])
+  useEffect(()=> {
+
+  }, [])
+
   return (
     <>
       <h1>Texto</h1>
