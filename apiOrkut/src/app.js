30a4b44bf593a40cd4afcb849dd911e3b2ee6d81
laecio22 <laecon22@hotmail.com>
2026-03-24 19:40:41 -0300

    feat:implementação projeto apiOrkut  integrando  com  os  dados  do banco  de dados postgres

---
+++ apiOrkut/src/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const pool = require("./config/db");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("<h1>Rede Social!</h1>");
+});
+
+app.get("/posts", async (req, res) => {
+  try {
+    const resultado = await pool.query(`
+        SELECT 
+          usuarios.nome,
+          post.conteudo,
+          post.criado_em
+
+          FROM post
+          JOIN usuarios
+          ON post.usuario_id = usuarios.id
+          ORDER BY post.criado_em DESC
+        `);
+
+    res.json(resultado.rows);
+  } catch (error) {
+    res.status(500).json({
+      erro: "Erro  ao buscar   postagens",
+    });
+  }
+});
+
+module.exports = app;
